9e872e2a3c8632e8baaf7f952d65e66160e71786
lilyCuZn <culily02@gmail.com>
2024-11-09 22:25:06 +0800

    init

---
+++ database_project/web/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+class tmp(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)

de901f0e350b1c3fd64516cb1487e4a278a5e6b8
Foolish-Han <nmdqbcmz@gmail.com>
2024-12-10 21:19:51 +0800

    Successfully deployed

--- FrontEnd/vue.config.js
+++ FrontEnd/vue.config.js
@@ -1,15 +1,17 @@
 module.exports = {
   devServer: {
-    proxy: {
-      "/web": {
-        target: "http://127.0.0.1:8000",
+	host: '0.0.0.0',
+	  port:8080,
+    	proxy: {
+      		"/web": {
+    			target: "http://59.110.236.209:8000",
         // 59.110.236.209
         // 10.193.115.252
-        changeOrigin: true,
-        pathRewrite: {
-          "^": "",
-        },
-      },
-    },
+      			changeOrigin: true,
+       			pathRewrite: {
+		          "^": "",
+		        },
+     		},
+   	},
   },
 };
